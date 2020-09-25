6670d7aba0b4fc8d6c2a4462c1b8dfad7080ce10
Mingjian Wen <wenxx151@gmail.com>
2020-09-24 21:59:06 -0700

    Add reaction encoder

---
+++ rxnrep/model/encoder.py
@@ -0,0 +1,262 @@
+import torch
+import logging
+import dgl
+import torch.nn as nn
+from typing import Dict, List, Optional
+from rxnrep.model.gatedconv import GatedGCNConv, GatedGCNConv1, GatedGCNConv2
+from rxnrep.model.utils import UnifySize
+
+logger = logging.getLogger(__name__)
+
+
+class ReactionEncoder(nn.Module):
+    """
+    Representation of a reaction.
+
+    This is achieve by:
+
+    1. update atom, bond, and global features of molecule graphs for the reactants and
+       products with gated graph conv layers.
+    2. compute the difference features between the products and the reactants.
+    3. update the difference features obtained in step 2 of the reaction graph with gated
+       graph conv layers.
+
+    Args:
+
+        embedding_size: Typically, atom, bond, and global features do not have the same
+            size. We apply a linear layer to unity their size to `embedding_size`. If
+            `None`, the embedding layer is not applied.
+        molecule_conv_layer_sizes: Hidden layer size of the graph conv layers for molecule
+            feature update. There will be `len(molecule_hidden_size)` graph conv layers.
+    """
+
+    def __init__(
+        self,
+        in_feats: Dict[str, int],
+        embedding_size: Optional[int] = None,
+        molecule_conv_layer_sizes: Optional[List[int]] = None,
+        molecule_num_fc_layers: Optional[int] = 2,
+        molecule_graph_norm: Optional[bool] = False,
+        molecule_batch_norm: Optional[bool] = True,
+        molecule_activation: Optional[str] = "ReLU",
+        molecule_residual: Optional[bool] = True,
+        molecule_dropout: Optional[float] = 0.0,
+        reaction_conv_layer_sizes: Optional[List[int]] = None,
+        reaction_num_fc_layers: Optional[int] = 2,
+        reaction_graph_norm: Optional[bool] = False,
+        reaction_batch_norm: Optional[bool] = True,
+        reaction_activation: Optional[str] = "ReLU",
+        reaction_residual: Optional[bool] = True,
+        reaction_dropout: Optional[float] = 0.0,
+        conv="GatedGCNConv",
+    ):
+        super(ReactionEncoder, self).__init__()
+
+        # assign default values
+        if molecule_conv_layer_sizes is None:
+            molecule_conv_layer_sizes = [64, 64, 32]
+        if isinstance(molecule_activation, str):
+            molecule_activation = getattr(nn, molecule_activation)()
+        if reaction_conv_layer_sizes is None:
+            reaction_conv_layer_sizes = [64, 64, 32]
+        if isinstance(reaction_activation, str):
+            reaction_activation = getattr(nn, reaction_activation)()
+
+        # unify atom, bond, and global feature size
+        if embedding_size is not None:
+            self.embedding = UnifySize(in_feats, embedding_size)
+            in_size = embedding_size
+        else:
+            if len(set(in_feats.values())) != 1:
+                raise ValueError(
+                    f"Expect `in_feats` values be equal to each; got {in_feats}. "
+                    "You probably need to set `embedding_size`."
+                )
+            self.embedding = nn.Identity()
+            in_size = list(in_feats.keys())[0]
+
+        # graph conv layer type
+        if conv == "GatedGCNConv":
+            conv_fn = GatedGCNConv
+        elif conv == "GatedGCNConv1":
+            conv_fn = GatedGCNConv1
+        elif conv == "GatedGCNConv2":
+            conv_fn = GatedGCNConv2
+        else:
+            raise ValueError()
+
+        # graph conv layers to update features of molecule graph
+        self.molecule_conv_layers = nn.ModuleList()
+        for layer_size in molecule_conv_layer_sizes:
+            self.molecule_conv_layers.append(
+                conv_fn(
+                    input_dim=in_size,
+                    output_dim=layer_size,
+                    num_fc_layers=molecule_num_fc_layers,
+                    graph_norm=molecule_graph_norm,
+                    batch_norm=molecule_batch_norm,
+                    activation=molecule_activation,
+                    residual=molecule_residual,
+                    dropout=molecule_dropout,
+                )
+            )
+            in_size = layer_size
+
+        # graph conv layers to update features of reaction graph
+        self.reaction_conv_layers = nn.ModuleList()
+        for layer_size in reaction_conv_layer_sizes:
+            self.reaction_conv_layers.append(
+                conv_fn(
+                    input_dim=in_size,
+                    output_dim=layer_size,
+                    num_fc_layers=reaction_num_fc_layers,
+                    graph_norm=reaction_graph_norm,
+                    batch_norm=reaction_batch_norm,
+                    activation=reaction_activation,
+                    residual=reaction_residual,
+                    dropout=reaction_dropout,
+                )
+            )
+            in_size = layer_size
+
+    def forward(
+        self,
+        molecule_graphs: dgl.DGLGraph,
+        reaction_graphs: dgl.DGLGraph,
+        feats: Dict[str, torch.Tensor],
+        metadata: Dict[str, List[int]],
+        norm_atom=None,
+        norm_bond=None,
+    ) -> Dict[str, torch.Tensor]:
+        """
+        Args:
+            molecule_graphs: (batched) dgl graphs for reactants and products
+            reaction_graphs: (batched) dgl graphs for reactions
+            feats: (batched) `atom`, `bond`, and `global` features for the molecules.
+                The atom features have a shape
+                of (Na, D), the bond features have a shape of (Nb, D), and the global
+                features have a shape of (Ng, D), where Na is the total number of atom
+                nodes in all the reactants and products graphs, Nb is the total number
+                of bond nodes in all the reactants and products graphs, and Ng is the
+                total number of global nodes. Note that for one reaction. The number of
+                nodes for each node type for each reaction is given in the metadata.
+            metadata: holds the number of nodes for all reactions. The size of each
+                list is equal to the number of reactions, and element `i` gives the info
+                for reaction `i`.
+            norm_atom: (2D tensor or None) graph norm for atom
+            norm_bond: (2D tensor or None) graph norm for bond
+
+        Returns:
+            Atom, bond, and global features for the reaction, of shape (Na, D'), (Nb', D')
+            and (Ng', D'), respectively. The number of the atom features stay the same,
+            while the number of bond and global features changes. This happens when we
+            assemble the molecules graphs to reaction graphs. Specifically, the number
+            of bond nodes for each reaction is the union of the unchanged bonds,
+            lost bonds in reactants, and added bonds in products, and the number of
+            global nodes become 1 for each reaction.
+        """
+
+        # embedding
+        feats = self.embedding(feats)
+
+        # molecule graph conv layer
+        for layer in self.molecule_conv_layers:
+            feats = layer(molecule_graphs, feats, norm_atom, norm_bond)
+
+        # create difference reaction features from molecule features
+        diff_feats = create_reaction_features(feats, metadata)
+
+        # reaction graph conv layer
+        feats = diff_feats
+        for layer in self.reaction_conv_layers:
+            feats = layer(reaction_graphs, feats, norm_atom, norm_bond)
+
+        return feats
+
+
+def create_reaction_features(
+    molecule_feats: Dict[str, torch.Tensor], metadata: Dict[str, List[int]]
+) -> Dict[str, torch.Tensor]:
+    """
+    Compute the difference features between the products and the reactants.
+
+    Atom features of the reactants are subtracted from that of the products.
+
+    Bonds features have three parts. For unchanged bonds, reactants features are
+    subtracted from the products. For lost bonds in the reactants, the negative of the
+    features are used. For added bonds in the products, the features are directly copied.
+
+    Each reaction has 1 global reactions: computed as the difference of the mean of the
+    global features between the products and the reactants.
+
+    Args:
+        molecule_feats: (batched) features for the molecules.
+        metadata: holds the number of nodes for all reactions. The size of each
+            list is equal to the number of reactions, and element `i` gives the info
+            for reaction `i`.
+
+    Returns:
+        Difference features between the products and the reactants.
+        Will have atom and global features, but not bond features.
+        Atom and global feature tensors should have the same shape as the reactant
+        feature tensors.
+    """
+
+    atom_feats = molecule_feats["atom"]
+    bond_feats = molecule_feats["bond"]
+    global_feats = molecule_feats["global"]
+
+    # Atom difference feats
+    size = len(atom_feats) // 2  # same number of atom nodes in reactants and products
+    reactant_atom_feats = atom_feats[:size]
+    product_atom_feats = atom_feats[size:]
+    diff_atom_feats = product_atom_feats - reactant_atom_feats
+
+    # Bond difference feats
+    total_num_reactant_bonds = sum(metadata["reactant_num_bonds"])
+    reactant_bond_feats = bond_feats[:total_num_reactant_bonds]
+    product_bond_feats = bond_feats[total_num_reactant_bonds:]
+
+    # feats of each reactant (product), list of 2D tensor
+    reactant_bond_feats = torch.split(reactant_bond_feats, metadata["reactant_num_bonds"])
+    product_bond_feats = torch.split(product_bond_feats, metadata["product_num_bonds"])
+
+    # calculate difference feats
+    diff_bond_feats = []
+    for i, (r_ft, p_ft) in enumerate(zip(reactant_bond_feats, product_bond_feats)):
+        num_unchanged_bond = metadata["num_unchanged_bonds"][i]
+        unchanged_bond_feats = p_ft[:num_unchanged_bond] - r_ft[:num_unchanged_bond]
+        lost_bond_feats = -r_ft[num_unchanged_bond:]
+        added_bond_feats = p_ft[num_unchanged_bond:]
+        feats = torch.cat([unchanged_bond_feats, lost_bond_feats, added_bond_feats])
+        diff_bond_feats.append(feats)
+    diff_bond_feats = torch.cat(diff_bond_feats)
+
+    # Global difference feats
+
+    total_num_reactant_molecules = sum(metadata["reactant_num_molecules"])
+    reactant_global_feats = global_feats[:total_num_reactant_molecules]
+    product_global_feats = global_feats[total_num_reactant_molecules:]
+
+    # the mean of global features in each reactant (product) graph
+    # Note, each reactant (product) graph holds all the molecules in the
+    # reactant (products), and thus has multiple global features.
+    mean_reactant_global_feats = dgl.ops.segment.segment_reduce(
+        torch.tensor(metadata["reactant_num_molecules"]),
+        reactant_global_feats,
+        reducer="mean",
+    )
+    mean_product_global_feats = dgl.ops.segment.segment_reduce(
+        torch.tensor(metadata["product_num_molecules"]),
+        product_global_feats,
+        reducer="mean",
+    )
+    diff_global_feats = mean_product_global_feats - mean_reactant_global_feats
+
+    diff_feats = {
+        "atom": diff_atom_feats,
+        "bond": diff_bond_feats,
+        "global": diff_global_feats,
+    }
+
+    return diff_feats
---
+++ rxnrep/tests/model/test_encoder.py
@@ -0,0 +1,57 @@
+import torch
+import numpy as np
+from rxnrep.model.encoder import create_reaction_features
+
+
+def test_create_reaction_features():
+    """
+    We assume two reactions (bond index inside parenthesis are its feature index):
+
+      0(0)   1(1)       0(5)  1(6)
+    C----C  C----C  -> C----C----C + C
+      m0       m1        m3          m4
+
+     0(2) 2(4)  1(3)       0(7)     1(8)
+    C----C----C----C  -> C----C + C----C
+          m2               m5       m6
+
+    """
+    metadata = {
+        "reactant_num_molecules": [2, 1],
+        "product_num_molecules": [2, 2],
+        "num_unchanged_bonds": [1, 2],
+        "reactant_num_bonds": [2, 3],
+        "product_num_bonds": [2, 2],
+    }
+
+    # feats
+    atom_feats = torch.from_numpy(np.arange(16 * 2).reshape(16, 2).astype(np.float32))
+    bond_feats = torch.from_numpy(np.arange(9 * 3).reshape(9, 3).astype(np.float32))
+    global_feats = torch.from_numpy(np.arange(7 * 4).reshape(7, 4).astype(np.float32))
+    ref_atom_feats = (
+        atom_feats[[8, 9, 10, 11, 12, 13, 14, 15]] - atom_feats[[0, 1, 2, 3, 4, 5, 6, 7]]
+    )
+    ref_bond_feats = torch.stack(
+        [
+            bond_feats[5] - bond_feats[0],
+            -bond_feats[1],
+            bond_feats[6],
+            bond_feats[7] - bond_feats[2],
+            bond_feats[8] - bond_feats[3],
+            -bond_feats[4],
+        ]
+    )
+
+    ref_global_feats = torch.stack(
+        [
+            torch.mean(global_feats[3:5], dim=0) - torch.mean(global_feats[0:2], dim=0),
+            torch.mean(global_feats[5:7], dim=0) - global_feats[2],
+        ]
+    )
+
+    molecule_feats = {"atom": atom_feats, "bond": bond_feats, "global": global_feats}
+    diff_feats = create_reaction_features(molecule_feats, metadata)
+
+    assert torch.equal(diff_feats["atom"], ref_atom_feats)
+    assert torch.equal(diff_feats["bond"], ref_bond_feats)
+    assert torch.equal(diff_feats["global"], ref_global_feats)
