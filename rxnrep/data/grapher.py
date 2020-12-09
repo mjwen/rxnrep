2e065c5920cf9c13978157fe88d5f51fa463ae3a
Mingjian Wen <wenxx151@gmail.com>
2020-12-08 17:44:08 -0800

    Add atom type masker

--- rxnrep/data/grapher.py
+++ rxnrep/data/grapher.py
@@ -4,10 +4,11 @@ Build molecule graphs.
 import copy
 import itertools
 from collections import defaultdict
-from typing import List, Optional
+from typing import List, Optional, Tuple
 
 import dgl
 import networkx as nx
+import numpy as np
 import torch
 from rdkit import Chem
 
@@ -574,3 +575,118 @@ def get_bond_distance_to_reaction_center(
     )
 
     return hop_distances
+
+
+class AtomTypeFeatureMasker:
+    """
+    A class to mask atom type features.
+
+    We mask the atom type (features) of certain atoms and let the NN to predict the
+    atom type based on its neighboring info so as to require the NN to learn and
+    retain atom type info in the representational vector.
+
+    If feature mean and std are provided, the mask features are set to -mean/std,
+    equivalent to setting all one hot encoding digits to 0 when getting the features.
+    Otherwise, the mask values are set to zeros.
+
+    Args:
+        allowable_types: all allowed atom types. The class labels for masked atoms are
+            generated from this.
+        feature_names: name of the atom features e.g. `atom type`, `in ring`....
+            This is used to find the indices of the atom features stored in the graphs.
+        feature_mean: 1D tensor. Mean of the atom features. This is used together with
+            `feature_std`, to determine the feature values for masked atoms.
+        feature_std: 1D tensor. Standard deviation of atom features.
+        ratio: ratio of atoms in each reaction to mask.
+    """
+
+    def __init__(
+        self,
+        allowable_types: List[str],
+        feature_names: List[str],
+        feature_mean: Optional[torch.Tensor] = None,
+        feature_std: Optional[torch.Tensor] = None,
+        ratio: float = 0.2,
+    ):
+        self.allowable_types = sorted(allowable_types)
+        self.ratio = ratio
+
+        self.class_labels_map = {s: i for i, s in enumerate(self.allowable_types)}
+
+        # indices of atom type features
+        self.start_index = feature_names.index("atom type")
+        self.end_index = len(feature_names) - feature_names[::-1].index("atom_type")
+
+        if feature_mean is not None and feature_std is not None:
+            # set mask values to be - mean/std. This is equivalent to set all values of
+            # the one hot encoding to 0
+            mean = feature_mean[self.start_index : self.end_index]
+            std = feature_std[self.start_index : self.end_index]
+            self.mask_values = -mean / std
+        else:
+            self.mask_values = torch.zeros(self.end_index - self.start_index)
+
+    def mask_features(
+        self, reactants_g: dgl.DGLGraph, products_g: dgl.DGLGraph, reaction: Reaction
+    ) -> Tuple[dgl.DGLGraph, dgl.DGLGraph, List[bool], List[int]]:
+        """
+        Make the atom type features.
+
+        Args:
+            reactants_g: reactants graph
+            products_g: products graph
+            reaction: the reaction
+
+        Returns:
+            reactants_g: the reactants graph with atom type feature updated
+            products_g: the products graph with atom type feature updated
+            is_atom_masked: whether an atom is masked or not. Should have a length of
+                number of atoms in the reaction.
+            masked_atom_labels: class label for the masked atoms. Should have a length
+                of number of masked atoms as indicated in `masked`. The order of the
+                labels are in correspondence with `is_atom_masked`. For example, suppose
+                `is_atoms_masked = [True, False, True, False, False]` (i.e. atoms 0 and
+                2 are masked), then `masked_atom_labels = [1, 5]` means the label for
+                atom 0 is 1 and for atom 2 is 5.
+        """
+
+        num_atoms = sum([m.num_atoms for m in reaction.reactants])
+        masked_atoms = np.random.permutation(num_atoms)[: int(num_atoms * self.ratio)]
+        masked_atoms = sorted(masked_atoms)
+
+        # number of atoms is small and no atom is masked
+        if not masked_atoms:
+            is_atom_masked = [False] * num_atoms
+            masked_atom_labels = []
+
+        # some atom is masked
+        else:
+            is_atom_masked = [
+                True if i in masked_atoms else False for i in range(num_atoms)
+            ]
+
+            # note `masked_atoms` are the map number of atoms
+            map_number = np.concatenate(
+                reaction.get_reactants_atom_map_number(zero_based=True)
+            ).tolist()
+            species = np.concatenate([m.species for m in reaction.reactants]).tolist()
+
+            masked_atom_labels = []
+            for atom in masked_atoms:
+
+                # set masked atom labels
+                idx = map_number.index(atom)
+                s = species[idx]
+                lb = self.class_labels_map[s]
+                masked_atom_labels.append(lb)
+
+                # update atom features
+                reactants_g["atom"].data["feat"][atom][
+                    self.start_index : self.end_index
+                ] = self.mask_values
+
+                products_g["atom"].data["feat"][atom][
+                    self.start_index : self.end_index
+                ] = self.mask_values
+
+        return reactants_g, products_g, is_atom_masked, masked_atom_labels
