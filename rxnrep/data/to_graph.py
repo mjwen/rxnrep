9154a98a34acb93f491822f9693ff676373e58ad
Mingjian Wen <wenxx151@gmail.com>
2021-03-03 17:56:00 -0800

    Add build graph, where bond is treated as edge

---
+++ rxnrep/data/to_graph.py
@@ -0,0 +1,65 @@
+"""
+Build molecule graphs.
+"""
+
+import dgl
+
+from rxnrep.core.molecule import Molecule
+
+
+def mol_to_graph(
+    mol: Molecule, num_virtual_nodes: int = 0, self_loop: bool = False
+) -> dgl.DGLGraph:
+    """
+    Create molecule graph for an rdkit molecule.
+
+    Atoms will be nodes (with node type `atom`) of the graph; atom i be node i.
+    Bonds will be edges (with edge type `a2a`, i.e. atom to atom). The is a bi-directed
+    graph and each bond corresponds to two edges: bond j corresponds to edges 2j and
+    2j+1.
+
+    if `num_virtual_node > 0`, the corresponding number of virtual nodes
+    are created (with node type 'virtual'). Each virtual node is connected to all
+    the atom nodes via virtual edges (with edge type `a2v` and `v2a`).
+
+    Args:
+        mol: molecule
+        num_virtual_nodes: number of virtual nodes to add. e.g. node holding global
+            features of molecules.
+        self_loop: whether to create self loop for atom nodes, i.e. add an edge for each
+            atom node connecting to it self.
+
+    Returns:
+        dgl graph for the molecule
+    """
+
+    edges_dict = {}
+
+    # atom to atom nodes
+    a2a = []
+    for i, j in mol.bonds:
+        a2a.extend([[i, j], [j, i]])
+
+    # atom self loop nodes
+    if self_loop:
+        a2a.extend([[i, i] for i in range(mol.num_atoms)])
+
+    edges_dict[("atom", "a2a", "atom")] = a2a
+    num_nodes = {"atom": mol.num_atoms}
+
+    # virtual nodes
+    if num_virtual_nodes > 0:
+        a2v = []
+        v2a = []
+        for a in range(mol.num_atoms):
+            for v in range(num_virtual_nodes):
+                a2v.append([a, v])
+                v2a.append([v, a])
+
+        edges_dict[("atom", "a2v", "virtual")] = a2v
+        edges_dict[("virtual", "v2a", "atom")] = v2a
+        num_nodes["virtual"] = num_virtual_nodes
+
+    g = dgl.heterograph(edges_dict, num_nodes_dict=num_nodes)
+
+    return g
---
+++ rxnrep/tests/data/test_to_graph.py
@@ -0,0 +1,110 @@
+import torch
+
+from rxnrep.core.molecule import Molecule
+from rxnrep.data.to_graph import mol_to_graph
+
+
+def create_graph(m, n_a, n_b, n_v, self_loop):
+    """
+
+    Args:
+        m: molecule
+        n_a:  number of atoms
+        n_b:  number of bonds
+        n_v:  number of virtual nodes
+    """
+
+    g = mol_to_graph(m, num_virtual_nodes=n_v, self_loop=self_loop)
+
+    g.nodes["atom"].data.update({"feat": torch.arange(n_a * 4).reshape(n_a, 4)})
+
+    bond_feats = torch.arange(n_b * 3).reshape(n_b, 3)
+    bond_feats = torch.repeat_interleave(
+        bond_feats, torch.tensor([2] * n_b).long(), dim=0
+    )
+    g.edges["a2a"].data.update({"feat": bond_feats})
+    if n_v > 0:
+        g.nodes["virtual"].data.update({"feat": torch.arange(n_v * 2).reshape(n_v, 2)})
+
+    return g
+
+
+def create_graph_C(num_virtual_nodes, self_loop=False):
+    """
+    Create a single atom molecule C.
+
+    atom_feats:
+        [[0,1,2,3]]
+
+    bond_feats:
+        None
+
+    virtual_feats:
+        [[0,1],
+         [2,3],
+         ...]
+    """
+    smi = "[C]"
+    m = Molecule.from_smiles(smi)
+
+    return create_graph(m, 1, 0, num_virtual_nodes, self_loop)
+
+
+def create_graph_CO2(num_virtual_nodes, self_loop=False):
+    """
+    Create a CO2 and add features.
+
+    Molecule:
+          0        1
+    O(0) --- C(1) --- O(2)
+
+    atom_feat:
+        [[0,1,2,3],
+         [4,5,6,7],
+         [8,9,10,11]]
+
+    bond_feat:
+        [[0,1,2],
+         [0,1,2],
+         [3,4,5],
+         [3,4,5]]
+    Note [0,1,2] corresponds to two edges for the same bond.
+
+
+    global_feat:
+        [[0,1],
+         [2,3],
+         ... ]
+
+    """
+    smi = "O=C=O"
+    m = Molecule.from_smiles(smi)
+
+    return create_graph(m, 3, 2, num_virtual_nodes, self_loop)
+
+
+def test_create_graph():
+    def assert_one(g, n_a, n_b, n_v):
+
+        num_nodes = {"atom": n_a}
+        num_edges = {"a2a": 2 * n_b}
+
+        if n_v > 0:
+            num_nodes["virtual"] = n_v
+            num_edges["v2a"] = n_a * n_v
+            num_edges["a2v"] = n_a * n_v
+
+        assert set(g.ntypes) == set(num_nodes.keys())
+        assert set(g.etypes) == set(num_edges.keys())
+        for k, n in num_nodes.items():
+            assert g.number_of_nodes(k) == n
+        for k, n in num_edges.items():
+            assert g.number_of_edges(k) == n
+
+    for n_v in range(3):
+        g = create_graph_C(num_virtual_nodes=n_v)
+        assert_one(g, 1, 0, n_v)
+
+    for n_v in [1]:
+        g = create_graph_CO2(num_virtual_nodes=n_v)
+        assert_one(g, 3, 2, n_v)
