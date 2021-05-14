3cb05e534b9e0fac1f0d326b8b65680c9d919eea
Mingjian Wen <wenxx151@gmail.com>
2021-05-13 22:09:09 -0700

    Use featurizer with partial charge for electrolyte ts dataset

--- rxnrep/data/electrolyte.py
+++ rxnrep/data/electrolyte.py
@@ -1,21 +1,35 @@
 import logging
 from collections import Counter
-from pathlib import Path
-from typing import Any, Dict, Optional, Union
+from typing import Dict, Optional
 
 import torch
 from sklearn.utils import class_weight
 
-from rxnrep.data.datamodule import BaseDataModule
 from rxnrep.data.featurizer import (
     AtomFeaturizerMinimum2,
+    AtomFeaturizerMinimum2AdditionalInfo,
     BondFeaturizerMinimum,
+    BondFeaturizerMinimumAdditionalInfo,
     GlobalFeaturizer,
 )
 from rxnrep.data.io import read_mrnet_reaction_dataset
-from rxnrep.data.uspto import BaseLabelledDataset
+from rxnrep.data.uspto import BaseClassificationDataModule, BaseLabelledDataset
 
 logger = logging.getLogger(__name__)
+
+
+def read_electrolyte_dataset(filename, nprocs):
+    logger.info("Start reading dataset file...")
+
+    succeed_reactions, failed = read_mrnet_reaction_dataset(filename, nprocs)
+
+    counter = Counter(failed)
+    logger.info(
+        f"Finish reading reactions. Number succeed {counter[False]}, "
+        f"number failed {counter[True]}."
+    )
+
+    return succeed_reactions, failed
 
 
 class ElectrolyteDataset(BaseLabelledDataset):
@@ -103,7 +117,7 @@ class ElectrolyteClassificationDataset(BaseLabelledDataset):
         if class_weight_as_1:
             w = torch.ones(num_reaction_classes)
         else:
-            rxn_classes = [rxn.get_property("class label") for rxn in self.reactions]
+            rxn_classes = [rxn.get_property("reaction_type") for rxn in self.reactions]
 
             # class weight for reaction classes
             w = class_weight.compute_class_weight(
@@ -118,48 +132,16 @@ class ElectrolyteClassificationDataset(BaseLabelledDataset):
         return weight
 
 
-class ElectrolyteClassificationDataModule(BaseDataModule):
+class ElectrolyteClassificationDataModule(BaseClassificationDataModule):
     """
-    Uspto dataset.
+    Electrolyte classification featurizer.
     """
-
-    def __init__(
-        self,
-        trainset_filename: Union[str, Path],
-        valset_filename: Union[str, Path],
-        testset_filename: Union[str, Path],
-        *,
-        state_dict_filename: Union[str, Path] = "dataset_state_dict.yaml",
-        restore_state_dict_filename: Optional[Union[str, Path]] = None,
-        batch_size: int = 100,
-        num_workers: int = 0,
-        pin_memory: bool = True,
-        num_processes: int = 1,
-        build_reaction_graph: bool = True,
-        num_reaction_classes: int = 2,
-    ):
-        super().__init__(
-            trainset_filename,
-            valset_filename,
-            testset_filename,
-            state_dict_filename=state_dict_filename,
-            restore_state_dict_filename=restore_state_dict_filename,
-            batch_size=batch_size,
-            num_workers=num_workers,
-            pin_memory=pin_memory,
-            num_processes=num_processes,
-            build_reaction_graph=build_reaction_graph,
-        )
-
-        self.num_reaction_classes = num_reaction_classes
 
     def setup(self, stage: Optional[str] = None):
 
         init_state_dict = self.get_init_state_dict()
 
-        atom_featurizer = AtomFeaturizerMinimum2()
-        bond_featurizer = BondFeaturizerMinimum()
-        global_featurizer = GlobalFeaturizer(allowable_charge=[-1, 0, 1])
+        atom_featurizer, bond_featurizer, global_featurizer = self._get_featurizers()
 
         self.data_train = ElectrolyteClassificationDataset(
             filename=self.trainset_filename,
@@ -199,29 +181,22 @@ class ElectrolyteClassificationDataModule(BaseDataModule):
         # save dataset state dict
         self.data_train.save_state_dict_file(self.state_dict_filename)
 
-    def get_to_model_info(self) -> Dict[str, Any]:
-        class_weight = self.data_train.get_class_weight(
-            num_reaction_classes=self.num_reaction_classes, class_weight_as_1=True
-        )
+    def _get_featurizers(self):
+        atom_featurizer = AtomFeaturizerMinimum2()
+        bond_featurizer = BondFeaturizerMinimum()
+        global_featurizer = GlobalFeaturizer(allowable_charge=[-1, 0, 1])
 
-        d = {
-            "feature_size": self.data_train.feature_size,
-            "num_reaction_classes": self.num_reaction_classes,
-            "reaction_class_weight": class_weight["reaction_type"],
-        }
-
-        return d
+        return atom_featurizer, bond_featurizer, global_featurizer
 
 
-def read_electrolyte_dataset(filename, nprocs):
-    logger.info("Start reading dataset file...")
+class ElectrolyteClassificationDataModule2(ElectrolyteClassificationDataModule):
+    """
+    Classification datamodule using additional feats, like partial charge.
+    """
 
-    succeed_reactions, failed = read_mrnet_reaction_dataset(filename, nprocs)
+    def _get_featurizers(self):
+        atom_featurizer = AtomFeaturizerMinimum2AdditionalInfo()
+        bond_featurizer = BondFeaturizerMinimumAdditionalInfo()
+        global_featurizer = GlobalFeaturizer(allowable_charge=[-1, 0, 1])
 
-    counter = Counter(failed)
-    logger.info(
-        f"Finish reading reactions. Number succeed {counter[False]}, "
-        f"number failed {counter[True]}."
-    )
-
-    return succeed_reactions, failed
+        return atom_featurizer, bond_featurizer, global_featurizer
